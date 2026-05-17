c05f513d1bd698500e75d130859c14761ec7318d
Alex Devlin <acdevlin42@gmail.com>
2026-05-17 16:10:29 -0700

    Preparation for Routing
    Adds react-router-dom and establishes basic scaffolding for routing.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import { describe, expect, test } from 'vitest';
 import { render } from 'vitest-browser-react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App.tsx';
 
 describe('App', () => {
   test('loads successfully', async () => {
-    const screen = await render(<App />);
+    const screen = await render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
     await expect(screen).toBeDefined();
     await expect(screen.getByText(/Get started/i)).toBeInTheDocument();
   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <Homepage />;
+  return (
+    <>
+      <Routes>
+        <Route index path="/" element={<Homepage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
